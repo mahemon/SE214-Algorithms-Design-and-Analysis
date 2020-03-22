94ca52c9360bfb1772b24a3e6184e32d1ff59181
Md Anwar Hossen <anwarcsejnu@gmail.com>
2020-03-22 17:30:34 +0600

    Add files via upload

--- GraphAlgorithms_BFSDFS/src/BFSDFS/BFS.java
+++ GraphAlgorithms_BFSDFS/src/BFSDFS/BFS.java
@@ -8,24 +8,24 @@ public class BFS {
         ReadFromFile read;
         public BFS(){
            g=new Graph(false); //true for directed.
-           ReadFromFile read=new ReadFromFile();
+           read=new ReadFromFile();
         }
         
         public void readGraph(){
             read.OpenFile("src/BFSDFS/bfs.txt");
             String nVertx;
-            int nVert = Integer.parseInt(read.Read()); //reading total number of vertices
+            int nVert = Integer.parseInt(read.Read()); //reading total number of vertices 8
             //System.out.println(nVert);
             for(int i=0;i<nVert;i++){ //reading lebel of vertices
             // System.out.println("   "+i+"  ");
-                nVertx = read.Read();
+                nVertx = read.Read(); // S R ..... 
                 g.addVertex(nVertx);
             }
             int nEdge = Integer.parseInt(read.Read());//reading number of edges
             for(int i=0;i<nEdge;i++){
-                // start node-end node of an edge
-                String split[]=(read.Read()).split("-");
-                g.addEdge(split[0],split[1]);
+                // start node-end node of an edge  R-S
+                String sp[]=(read.Read()).split("-");   // Split [0] = R  split[1]=S
+                g.addEdge(sp[0],sp[1]);
             }
         }
         public void doBFS(){   
